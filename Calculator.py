14eee88304cb9b8ab6c58b316262a31643299e57
Obijunior <65624828+Obijunior@users.noreply.github.com>
2020-11-23 10:29:52 -0600

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+num1 = input("First number")
+num2 = input("Second number")
+Operator = input("Operator(+*/-)")
+
+if Operator = "+"
+answer = num1+num2
+    print(answer)
+
+elif Operator = "-"
+answer = num1 - num2
+    print(answer)
+
+elif Operator = "*"
+answer = num1*num2
+    print(answer)
+
+elif Operator = "/"
+answer = num1/num2
+    print(answer)
+
+else :
+    print("Error")
