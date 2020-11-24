41d661c4ff4a39735f854f10f77849fd9c77379f
Obijunior <65624828+Obijunior@users.noreply.github.com>
2020-11-24 12:17:53 -0600

    Add files via upload
    Big Update
    Lots of UX changes, plus it actually works!

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,30 @@
-num1 = input("First number")
-num2 = input("Second number")
-Operator = input("Operator(+*/-)")
+def calculator() :
+    num1 = int(input("First number : "))
+    Operator = input("Operator(+  *  /  -  **[squared]) : ")
+    num2 = int(input("Second number : "))
 
-if Operator = "+"
-answer = num1+num2
-    print(answer)
+    if Operator == "+" :
+        answer = num1+num2
+        print('The answer is ' + str(answer) + '.')
 
-elif Operator = "-"
-answer = num1 - num2
-    print(answer)
+    elif Operator == "-" :
+        answer = num1 - num2
+        print('The answer is ' + str(answer) + '.')
 
-elif Operator = "*"
-answer = num1*num2
-    print(answer)
+    elif Operator == "*" :
+        answer = num1*num2
+        print('The answer is ' + str(answer) + '.')
 
-elif Operator = "/" 
-answer = num1/num2
-    print(answer)
+    elif Operator == "/" :
+        answer = num1/num2
+        print('The answer is ' + str(answer) + '.')
 
-else :
-    print("Error")
+    elif Operator == "**" :
+        answer = num1**num2
+        print('The answer is ' + str(answer) + '.')
+
+    else :
+        print("Error. The opperator you selected was invalid. Please try again")
+        calculator()
+
+calculator()
